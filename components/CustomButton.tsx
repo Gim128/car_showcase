f5ec110043d6176b8c131047b3c67f91e7e79703
Gim128 <gimhanpabasara4@gmail.com>
2024-07-14 19:40:32 +0530

    setting up here section

---
+++ components/CustomButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+
+"use-client";
+
+const CustomButton = () => {
+  return (
+    <button
+        disabled={false}
+        type={"button"}
+        className={`custom-btn`}
+        onClick={() => {}}
+        // 21:36
+    >
+      
+    </button>
+  )
+}
+
+export default CustomButton
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Image from 'next/image'
+import CustomButton from './CustomButton'
 
 const Hero = () => {
   return (
-    <div>
-      Hero
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        <h1 className='hero__title'>
+            Find, Book or Rent a Car Quickly.
+        </h1>
+
+        <p className='hero__subtitle'>
+            Streamline your car rental experience with our effortless booking process.
+        </p>
+
+        <CustomButton />
+      </div>
     </div>
   )
 }
